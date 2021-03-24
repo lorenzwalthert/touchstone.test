c4e410f9d96873d2d00686650f7a38c778db8fb5
Lorenz <lorenz.walthert@icloud.com>
2021-03-25 00:47:17 +0100

    drop refs / partial

--- touchstone/script.R
+++ touchstone/script.R
@@ -1,15 +1,13 @@
-timer <- purrr::partial(touchstone::benchmark_run_ref,
-  refs = refs, n = 2
+timer(
+  expr_before_benchmark = c("print(4)"),
+  expr1 = "tail(mtcars)",
+  n = 2
 )
 
 timer(
   expr_before_benchmark = c("print(4)"),
-  expr1 = "tail(mtcars)"
-)
-
-timer(
-  expr_before_benchmark = c("print(4)"),
-  expr2 = "head(mtcars)"
+  expr2 = "head(mtcars)",
+  n = 2
 )
 
 touchstone::benchmarks_analyse()
