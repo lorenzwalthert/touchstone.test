4a35b53113d3cfab8719b3602e22001e1570edd6
Lorenz <lorenz.walthert@icloud.com>
2021-03-25 00:28:47 +0100

    simplify script

--- touchstone/script.R
+++ touchstone/script.R
@@ -1,8 +1,3 @@
-refs <- c(
-  Sys.getenv("GITHUB_BASE_REF", "master"),
-  Sys.getenv("GITHUB_HEAD_REF", "master")
-)
-
 timer <- purrr::partial(touchstone::benchmark_run_ref,
   refs = refs, n = 2
 )
@@ -17,31 +12,4 @@ timer(
   expr2 = "head(mtcars)"
 )
 
-for (benchmark in touchstone::benchmark_ls()) {
-  timings <- touchstone::benchmark_read(benchmark, refs)
-
-  library(ggplot2)
-  library(magrittr)
-  timings %>%
-    ggplot(aes(x = elapsed, color = ref)) +
-    geom_density()
-  fs::path("touchstone/plots/", benchmark) %>%
-    fs::path_ext_set("png") %>%
-    ggsave()
-
-  tbl <- timings %>%
-    dplyr::group_by(.data$ref) %>%
-    dplyr::summarise(m = mean(.data$elapsed)) %>%
-    tibble::deframe()
-
-  diff_percent <- round(100 * (tbl[refs[2]] - tbl[refs[1]]) / tbl[refs[1]], 1)
-  cat(
-    glue::glue(
-      "{benchmark}: {round(tbl[refs[1]], 2)} -> {round(tbl[refs[2]], 2)} ",
-      "({diff_percent}%)"
-    ),
-    fill = TRUE,
-    file = "touchstone/pr-comment/info.txt",
-    append = TRUE
-  )
-}
+touchstone::benchmarks_analyse()
