5a4d4d5298469669de79988467c1c691337f79d3
Lorenz <lorenz.walthert@icloud.com>
2021-05-15 11:40:39 +0200

    add an integration test (kind of)

--- R/sample.R
+++ R/sample.R
@@ -1,1 +1,15 @@
-x <- 1
+#' This function brings a big speed improvement for the PR
+#' @export
+wait_long_for_head <- function(wait) {
+  base <- touchstone:::ref_get_or_fail("GITHUB_BASE_REF")
+  head <- touchstone:::ref_get_or_fail("GITHUB_HEAD_REF")
+  if (gert::git_branch() == head) {
+    Sys.sleep(wait)
+  } else if (gert::git_branch() == base) {
+    Sys.sleep(0)
+  } else {
+    rlang::abort(
+      "Currently branch must be either `GITHUB_BASE_REF` or `GITHUB_HEAD_REF`."
+    )
+  }
+}
--- touchstone/script.R
+++ touchstone/script.R
@@ -3,15 +3,23 @@ library(touchstone)
 refs_install()
 
 benchmark_run_ref(
-  expr_before_benchmark = c("print(4)"),
-  expr1 = "tail(mtcars)",
+  expr1 = c("touchstone.test::wait_long_for_head(4)"),
   n = 3
 )
 
 benchmark_run_ref(
-  expr_before_benchmark = c("print(4)"),
-  expr2 = "head(mtcars)",
+  expr2 = "Sys.sleep(1)",
   n = 3
 )
 
 benchmarks_analyze()
+
+bm_base <- benchmark_read("expr1", touchstone:::ref_get_or_fail("GITHUB_BASE_REF"))
+bm_head <- benchmark_read("expr1", touchstone:::ref_get_or_fail("GITHUB_HEAD_REF"))
+
+if (3 * mean(bm_base$elapsed) > mean(bm_head$elapsed)) {
+  rlang::abort(paste0(
+    "running benchmarks on head should be around 4 times slower.",
+    "They are not even 3 times slower. Something is suspicious."
+  ))
+}
