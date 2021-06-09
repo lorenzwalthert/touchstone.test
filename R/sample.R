5406d4a43e7da34c6d5a4cad60f7e41e0166585e
Lorenz <lorenz.walthert@icloud.com>
2021-06-09 21:29:11 +0200

    also install ref on remote

--- R/sample.R
+++ R/sample.R
@@ -25,6 +25,14 @@ install_use_push <- function(ref = "main") {
   )
   # old version might be loaded
   callr::r(function() touchstone::use_touchstone())
+  system2(
+    "sed", c(
+      "-i", "''", "-e",
+      '"s/remotes::install_github(\\\"lorenzwalthert\\/touchstone.*\\\").*/remotes::install_github(\\\"lorenzwalthert\\/touchstone@$R_TOUCHSTONE_TEST_REF\\\")/g"',
+      ".github/workflows/touchstone-receive.yaml"
+    ),
+    env = paste0("R_TOUCHSTONE_TEST_REF=", ref, ";")
+  )
   system2("git", c("add", "."))
   system2("git", c("commit", "-m", "use latest scripts", "--allow-empty"))
   gert::git_push()
